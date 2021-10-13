2ef74de5bf9b4a678475a7117486a9edb584f716
EngineerFardeenAbbasi <92368694+EngineerFardeenAbbasi@users.noreply.github.com>
2021-10-13 16:04:17 +0530

    Add files via upload

---
+++ UI.py.py
@@ -0,0 +1,17 @@
+from tkinter import *
+#from tkinter import font
+
+root=Tk()
+root.title("FACE RECOGNITION")
+root.geometry('400x200')
+root.minsize(100,50)
+#app name
+app_name=Label(text="FACE RECOGNITION",height='1',width='18',font=('courier',20))
+app_name.pack()
+#button for select the image in file
+btn=Button(text='select the \nimage',width='15',height='2',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
+btn.pack(side='top')
+btn=Button(text='open camera',width='15',height='2',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
+btn.pack(side='top')
+root.mainloop()
+
