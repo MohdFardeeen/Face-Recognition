14952f2934f48463ff125638195fcff6c5df5054
EngineerFardeenAbbasi <92368694+EngineerFardeenAbbasi@users.noreply.github.com>
2021-10-13 22:22:46 +0530

    Add files via upload

--- UI.py.py
+++ UI.py.py
@@ -9,9 +9,11 @@ root.minsize(100,50)
 app_name=Label(text="FACE RECOGNITION",height='1',width='18',font=('courier',20))
 app_name.pack()
 #button for select the image in file
-btn=Button(text='select the \nimage',width='15',height='2',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
+btn=Button(text='select the image',width='20',height='1',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
 btn.pack(side='top')
-btn=Button(text='open camera',width='15',height='2',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
+btn=Button(text='open camera',width='20',height='1',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
+btn.pack(side='top')
+btn=Button(text='select video',width='20',height='1',font=('comicsansms 12 bold'),fg='white',bg='gray',borderwidth=10)
 btn.pack(side='top')
 root.mainloop()
 
